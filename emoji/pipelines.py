b45e3fa31cfa186d4fac093132f4b1466aabac82
WadeTam <tangweimeng@live.com>
2019-09-29 11:50:38 +0800

    final version for Twemoji

--- emoji/pipelines.py
+++ emoji/pipelines.py
@@ -5,25 +5,29 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-import json
+
+import scrapy
+import urllib.parse
 import re
 from emoji.items import EmojiFile
 
 class EmojiPipeline(object):
-    def open_spider(self, spider):
-        self.file = open('../emoji_files.json', 'w')
+    emoji_url_base='https://commons.wikimedia.org/wiki/File:'
 
-    def close_spider(self, spider):
-        self.file.close()
+    # def open_spider(self, spider):
+    #     self.file = open('../emoji_files.json', 'w')
+
+    # def close_spider(self, spider):
+    #     self.file.close()
 
     def process_item(self, item, spider):
         # only handle EmojiFile item
-        if not isinstance(item, EmojiFile):
+        if isinstance(item, EmojiFile):
+            # extract emoji file name
+            if item.get('uri'):
+                item['name']=re.match(r'/wiki/File:(.*)',item['uri']).group(1)
+                # item['name']=item['uri'][len('/wiki/File:'):]
+
+                # go to the page of each emoji and save the emoji with given resolutions
+                yield scrapy.Request(urllib.parse.urljoin(self.emoji_url_base,item['name']),spider.open_emoji,meta={'filename':item['name']})
             return item
-        # extract emoji file name
-        if item.get('uri'):
-            item['name']=re.match(r'/wiki/File:(.*)',item['uri']).group(1)
-            # item['name']=item['uri'][len('/wiki/File:'):]
-        line = json.dumps(dict(item),ensure_ascii=False) + "\n"
-        self.file.write(line)
-        return item
--- emoji/spiders/twemoji.py
+++ emoji/spiders/twemoji.py
@@ -5,18 +5,16 @@ import json
 import logging
 from emoji.items import EmojiFile
 import urllib.parse
+import re
 
 
 class TwemojiSpider(scrapy.Spider):
     name = 'twemoji'
     resolution=[768,1024,1000,2000]
     size=[str(i)+'px' for i in resolution]
-    file_name=''
     pagination_base='https://commons.wikimedia.org'
-    emoji_url_base='https://commons.wikimedia.org/wiki/File:'
-    file_url_base='https://upload.wikimedia.org/wikipedia/commons/thumb/'
     start_urls = ['https://commons.wikimedia.org/w/index.php?title=Category:Twemoji_v2']
-    # start_urls = ['https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Twemoji2_1f170.svg/2000px-Twemoji2_1f170.svg.png']
+    # emoji_url_base='https://commons.wikimedia.org/wiki/File:'
 
     def __init__(self, *args, **kwargs):
         # disable yield in terminal
@@ -26,18 +24,48 @@ class TwemojiSpider(scrapy.Spider):
 
     def parse(self, response):
         # get all links to emoji file on current page
-        # emoji_links=response.css('a.galleryfilename-truncate::attr(href)').getall()
-        # for i in emoji_links:
-        #     e=EmojiFile(uri=i)
-        #     yield e
+        emoji_links=response.css('a.galleryfilename-truncate::attr(href)').getall()
+        for i in emoji_links:
+            file_name=re.match(r'/wiki/File:(.*)',i).group(1)
+            # file_name=i[len('/wiki/File:'):]
+
+            # go to the page of each emoji and save the emoji with given resolutions
+            yield scrapy.Request(urllib.parse.urljoin(self.pagination_base,i),self.open_emoji,meta={'filename':file_name})
 
         # exploit the structure of the page, the second element matched will only be either 'next page' or 'previous page'
         pagination_link=response.css('a[title="Category:Twemoji v2"]:nth-of-type(2)::attr(href)').get()
         pagination_text=response.css('a[title="Category:Twemoji v2"]:nth-of-type(2)::text').get()
-        # # recursively request all pages
+
+        # recursively request all pages
         if pagination_text=='next page':
-            print(pagination_text)
             yield scrapy.Request(urllib.parse.urljoin(self.pagination_base,pagination_link),self.parse)
 
-    def save_png():
-        pass
+    def open_emoji(self, response):
+        # actual_link=response.css('a.mw-thumbnail-link:nth-of-type(1)::attr(href)').get()
+
+        # # extract the file download link, 512px is the first occurence
+        # download_link=re.match(r'(.*)512px',actual_link).group(1)
+
+        # # download png file of each resolution
+        # for i in self.size:
+        #     url=i+'-'+response.meta['filename']+'.png'
+        #     request=urllib.parse.urljoin(download_link,url)
+        #     yield scrapy.Request(request,self.save_png,meta={'filename':url,'size':i})
+
+        # download svg file
+        svg_link=response.css('a.internal::attr(href)').get()
+        yield scrapy.Request(svg_link,self.save_svg,meta={'filename':response.meta['filename']})
+
+    def save_png(self, response):
+        path='pic/'+response.meta['size']
+        if not os.path.exists(path):
+            os.makedirs(path)
+        with open(os.path.join(path,response.meta['filename']),'wb') as f:
+            f.write(response.body)
+
+    def save_svg(self, response):
+        path='pic/svg'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        with open(os.path.join(path,response.meta['filename']),'wb') as f:
+            f.write(response.body)
